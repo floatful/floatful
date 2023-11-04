19878a35f539684ed002a34d235e606f8fcc6f97
HazelBall <roundnround01@gmail.com>
2023-11-03 21:21:50 -0700

    Update SelectorList to TSX

--- src/components/selectors/SelectorList.tsx
+++ src/components/selectors/SelectorList.tsx
@@ -1,25 +1,27 @@
 import React, { useState } from 'react'
 import CreateRule from './CreateRule';
+import Rule from '../../data/Rule';
 
-const SelectorList = ({rules, selectedRules, onRuleSelect, dispatch}) => {
+const SelectorList = (props:{rules:Rule[], selectedRules:Rule[], onRuleSelect:(e:React.MouseEvent<HTMLLIElement>, selector:String)=>{}, dispatch:({})=>{}}) => {
 
     const [createRuleModalStatus, setCreateRuleModalStatus] = useState(false);
 
-    const toggleCreateRuleModal = (bool) => {
-        if(bool !== createRuleModalStatus)
-            setCreateRuleModalStatus(bool);
+    const toggleCreateRuleModal:(close:boolean)=>{} = (close:boolean) => {
+        if(close !== createRuleModalStatus)
+            setCreateRuleModalStatus(close);
+        return {};
     }
 
     return (
         <div className = "floatful floatful--selectors">
-            {createRuleModalStatus && <CreateRule dispatch={dispatch} onClose={toggleCreateRuleModal}/>}
+            {createRuleModalStatus && <CreateRule dispatch={props.dispatch} onClose={toggleCreateRuleModal}/>}
             <h2>Rules</h2>
             <ul className = "floatful--selector-list">
-                {rules.map((rule) => (
+                {props.rules.map((rule:Rule) => (
 					<li 
                         className = "floatful--selector"
 						key = {rule.selector}
-                        onClick={(e) => {onRuleSelect(e, rule.selector)}}
+                        onClick={(e) => {props.onRuleSelect(e, rule.selector)}}
 					>
 						{rule.selector}
 					</li>
